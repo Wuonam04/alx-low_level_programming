30238bc133a9f0e7d5d50b0d85db487676fab01c
Wuonam04 <odeedward04@gmail.com>
2022-07-11 13:59:21 -0700

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - interchange the valus being held by two intergers
+* @a - parameter1
+* @b - parameter2
+*
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *b;
+*b = *a;
+*a = tmp;
+}
