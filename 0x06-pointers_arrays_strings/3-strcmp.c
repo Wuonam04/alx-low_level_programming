9c87dd7346ec97fad67379075af6758174d0067a
Wuonam04 <odeedward04@gmail.com>
2022-07-14 13:48:38 -0700

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp - compare string
+* @s1: string 1
+* @s2: string 2
+* Return: 0
+*
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	result = strcmp(s1, s2);
+	return (0);
+}
