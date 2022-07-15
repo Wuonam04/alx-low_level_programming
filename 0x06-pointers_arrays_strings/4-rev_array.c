2d226c037542216f7a6a0b4435ebdc75db11bb17
Wuonam04 <odeedward04@gmail.com>
2022-07-15 13:19:53 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,15 +11,12 @@ int *p, i, aux, k;
 
 p = a;
 for (i = 0; i < n; i++)
-	p++;
-for (k = 0, k < i / 2; k++)
+p++;
+for (k = 0; k < i / 2; k++)
 {
-
 aux = a[k];
 a[k] = *p;
 *p = aux;
-
 p--;
 }
-
 }
