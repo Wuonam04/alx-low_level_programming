d8f99c21f4932b67604dbadb8686c7d9b673647e
Wuonam04 <odeedward04@gmail.com>
2022-07-18 13:16:00 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _memcpy - memory copy function
+* @dest: destination to be copied to
+* @src: what is to be copied
+* @n: integer
+* Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+
+for (a = 0; a < n; a++)
+	dest[a] = src[a];
+
+return (dest);
+
+
+}
