3b59dddb0feff60eb9492a6fdabaef75c40fa901
Wuonam04 <odeedward04@gmail.com>
2022-07-18 12:51:31 -0700

    memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _memset - set memory function
+* @s: string
+* @b: character
+* @n: integer
+* Return: string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+	s[a] = b
+
+return (string);
+}
