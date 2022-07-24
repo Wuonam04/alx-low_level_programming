07421e8dcd2c18eee69b93027c8caf7eb3483f38
Wuonam04 <odeedward04@gmail.com>
2022-07-24 15:09:30 -0700

    prints name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints name
+* @argc: argument count
+* @argv: argument value
+*
+* Return: nothing
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
