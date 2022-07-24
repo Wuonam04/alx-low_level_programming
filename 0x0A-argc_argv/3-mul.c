a1fef342d18f85785b2f3c463c4f730f0c985245
Wuonam04 <odeedward04@gmail.com>
2022-07-24 15:36:38 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,7 +16,7 @@ if (argc < 3)
 printf("Error\n");
 return (1);
 }
-for (index = 1; inde < argc; index++)
+for (index = 1; index < argc; index++)
 {
 multiplication = multiplication *atoi(argv[index]);
 }
